e9d4b823ef402dd36676d0c9d63c077e314c27ae
Swaty-kw <dashtilimited@gmail.com>
2024-08-06 15:50:36 +0300

    done

---
+++ function2pt2.js/functions.2.js
@@ -0,0 +1,13 @@
+function isOdd(number) {
+  if (number % 2 == 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+let testnumber = 7;
+let resultEven = isOdd(testnumber);
+console.log(`${testnumber}is ${resultEven}`);
+
+function oddsSmallerThan(7)
+if 
